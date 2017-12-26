0c36408ee9eb34e5c78fb9d25f491ff92aba8216
Previous28 <shujh_sysu@163.com>
2017-12-26 13:57:08 +0800

    [增加] 增加了左侧菜单栏，版本1.0

---
+++ app/src/main/java/com/example/a77354/android_final_project/CircleImageView.java
@@ -0,0 +1,34 @@
+package com.example.a77354.android_final_project;
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+
+//import com.github.mzule.fantasyslide.app.R;
+
+/**
+ * Created by shujunhuai on 2017/12/26.
+ */
+
+public class CircleImageView extends de.hdodenhof.circleimageview.CircleImageView {
+    public CircleImageView(Context context) {
+        super(context);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void setPressed(boolean pressed) {
+        super.setPressed(pressed);
+        if (pressed) {
+            setBorderColor(getResources().getColor(R.color.blue));
+        } else {
+            setBorderColor(Color.WHITE);
+        }
+    }
+}
