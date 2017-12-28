ab6c5ffe87656fc17513bb4183e45e3d8861e862
Previous28 <shujh_sysu@163.com>
2017-12-28 09:02:42 +0800

    [修改] 修改了登录界面

--- app/src/main/java/com/example/a77354/android_final_project/LoginActivity.java
+++ app/src/main/java/com/example/a77354/android_final_project/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.example.a77354.android_final_project;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -33,5 +35,8 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
+        TextView register = (TextView) findViewById(R.id.register);
+        register.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
     }
 }
